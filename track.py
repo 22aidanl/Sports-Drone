a78fdd9aeea89a2885897f5a1a21bf84e60dd9ff
Aidan Lincke <aidanlincke@Aidans-MacBook-Air.local>
2021-07-26 11:32:05 -0700

    Fix track.py

--- track.py
+++ track.py
@@ -9,32 +9,31 @@ tello.streamon()
 while True:
     frame = tello.get_frame_read().frame
     if frame is not None:
-        # bballLower = np.array([0,20,70])
-        # bballUpper = np.array([30,50,110])
-        # mask = cv2.inRange(frame, bballLower, bballUpper)
-        # frame = cv2.bitwise_and(frame, frame, mask=mask)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        bballLower = np.array([0,50,0])
+        bballUpper = np.array([12,255,255])
+        mask = cv2.inRange(hsv, bballLower, bballUpper)
+        hsv = cv2.bitwise_and(hsv, hsv, mask=mask)
+        frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+        gray = cv2.split(frame)[2]
         gray = cv2.medianBlur(gray, 5)
         rows = gray.shape[0]
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
-                                param1=100, param2=30,
-                                minRadius=140, maxRadius=170)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 2,
+                                        param1=100, param2=30,
+                                        minRadius=20, maxRadius=170)
 
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
                 center = (i[0], i[1])
-                # circle center
-                # cv2.circle(frame, center, 1, (0, 100, 100), 3)
-                # circle outline
                 radius = i[2]
-                # cv2.circle(frame, center, radius, (255, 0, 255), 3)
+                cv2.circle(frame, center, radius, (255, 0, 255), 3)
 
         cv2.imshow("Tello Camera", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite("/Users/aidanlincke/Documents/Beaver Works/Sports-Drone/test2.jpg", frame)
             break
 
 tello.streamoff()
