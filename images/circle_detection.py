0d0767bb6cf5c7c468b0320b51358f81cf8087e9
Gaurvi Awasthi <gaurvi.awasthi@gmail.com>
2021-07-27 11:00:19 -0400

    Updated detecting circles

--- images/circle_detection.py
+++ images/circle_detection.py
@@ -1,34 +1,23 @@
 import cv2
 import numpy as np
-from djitellopy import tello
 import os
 
-#my_drone = tello.Tello()
-#my_drone.connect()
-#my_drone.streamon()
 
-#frame = my_drone.get_frame_read().frame
 frame = cv2.imread(os.path.dirname(os.path.realpath(__file__)) + '/1.jpg')
 #img = frame.copy()
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#new = cv2.GaussianBlur(gray, (15, 15), cv2.BORDER_DEFAULT)
 new = cv2.medianBlur(gray, 5)
-
 newimg = cv2.cvtColor(new, cv2.COLOR_GRAY2BGR)
-#cv2.imshow('Frame', img)
-#cv2.waitKey(1)
+
 ##CHANGE THE BELOW VALUES AS NEEDED##
 #for second image# circles = cv2.HoughCircles(new, cv2.HOUGH_GRADIENT, 1, 800, param1=10, param2=30, minRadius=0, maxRadius=250)
 circles = cv2.HoughCircles(new, cv2.HOUGH_GRADIENT, 1, 360, param1=100, param2=30, minRadius=0, maxRadius=250)
 circles = np.uint16(np.around(circles))
-        #print(circles)
-        #print(circles.shape)
 
 for i in circles[0, :]:
-    cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 3) ##CHANGE THESE VALUES AS NEEDED##
-    cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 10) ##CHANGE THESE VALUES AS NEEDED##
+    cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 3)
+    cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 10)
 
 cv2.imshow('Frame', frame)
-#cv2.imwrite("Circles.jpg", frame)
 cv2.waitKey(0)
---
+++ videos/circle_tracking.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+from djitellopy import tello
+
+my_drone = tello.Tello()
+my_drone.connect()
+my_drone.streamon()
+
+while True:
+    frame = my_drone.get_frame_read().frame
+    if frame is not None:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        new = cv2.medianBlur(gray, 5)
+        newimg = cv2.cvtColor(new, cv2.COLOR_GRAY2BGR)
+
+        circles = cv2.HoughCircles(new, cv2.HOUGH_GRADIENT, 1, 360, param1=100, param2=30, minRadius=0, maxRadius=250)
+        circles = np.uint16(np.around(circles))
+
+        for i in circles[0, :]:
+            cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 3)
+            cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 10)
+
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(0)
