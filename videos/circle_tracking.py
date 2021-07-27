0b4fda04c0a7c85fab032f11e4d2eac04ba50c33
Gaurvi Awasthi <gaurvi.awasthi@gmail.com>
2021-07-27 12:13:46 -0400

    Updated detecting circles

--- videos/circle_tracking.py
+++ videos/circle_tracking.py
@@ -6,12 +6,12 @@ my_drone = tello.Tello()
 my_drone.connect()
 my_drone.streamon()
 
-while True:
+'''while True:
     frame = my_drone.get_frame_read().frame
     if frame is not None:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         new = cv2.medianBlur(gray, 5)
-        newimg = cv2.cvtColor(new, cv2.COLOR_GRAY2BGR)
+        #newimg = cv2.cvtColor(new, cv2.COLOR_GRAY2BGR)
 
         circles = cv2.HoughCircles(new, cv2.HOUGH_GRADIENT, 1, 360, param1=100, param2=30, minRadius=0, maxRadius=250)
         circles = np.uint16(np.around(circles))
@@ -21,4 +21,23 @@ while True:
             cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 10)
 
         cv2.imshow('Frame', frame)
-        cv2.waitKey(0)
+        cv2.waitKey(0)'''
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, img = video_capture.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    circles1 = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 360, param1=100, param2=30, minRadius=0, maxRadius=250)
+    try:
+        circles = circles1[0, :, :]
+    except TypeError:
+        print('NULL')
+    else:
+        circles = np.uint16(np.around(circles))
+        for i in circles[:]:
+            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 3)
+            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 10)
+
+    cv2.imshow('Video', img)
