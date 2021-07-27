75631024e4d303b9514d3996b1c27d786acbf0c2
Aidan Lincke <22aidanl@students.harker.org>
2021-07-27 12:27:07 -0700

    Add Basketball class

--- images/contour_detection.py
+++ images/contour_detection.py
@@ -4,10 +4,10 @@ import os
 
 frame = cv2.imread(os.path.dirname(os.path.realpath(__file__)) + "/1.jpg")
 
-# Convert to HSV, use color ranges for a basketball, and convert back to BGR
+# Convert to HSV and use color ranges for a basketball
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-bballLower = np.array([0,150,150])
-bballUpper = np.array([15,255,255])
+bballLower = np.array([4,150,100])
+bballUpper = np.array([14,255,255])
 mask = cv2.inRange(hsv, bballLower, bballUpper)
 hsv = cv2.bitwise_and(hsv, hsv, mask=mask)
 
@@ -15,7 +15,7 @@ hsv = cv2.bitwise_and(hsv, hsv, mask=mask)
 gray = cv2.split(hsv)[2]
 gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, np.ones((15, 15)))
 contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(frame, contours, -1, (0, 255, 0), thickness=5)
 
+cv2.drawContours(frame, contours, -1, (0, 255, 0), thickness=5)
 cv2.imshow("Frame", frame)
 cv2.waitKey(0)
--- track_ball.py
+++ track_ball.py
@@ -1,4 +1,5 @@
 from pid import PIDController
+from basketball import Basketball
 from djitellopy import Tello
 import cv2
 import numpy as np
@@ -11,8 +12,8 @@ while True:
     frame = tello.get_frame_read().frame
     if frame is not None:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        bballLower = np.array([0,150,150])
-        bballUpper = np.array([15,255,255])
+        bballLower = np.array([4,200,150])
+        bballUpper = np.array([14,255,255])
         mask = cv2.inRange(hsv, bballLower, bballUpper)
         hsv = cv2.bitwise_and(hsv, hsv, mask=mask)
 
@@ -20,6 +21,10 @@ while True:
         gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, np.ones((15, 15)))
         contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(frame, contours, -1, (0, 255, 0), thickness=5)
+
+        bball = Basketball(contours)
+        if hasattr(bball, "centroid"):
+            cv2.circle(frame, bball.centroid, 5, (255, 0, 0), thickness=10)
 
         cv2.imshow("Tello Camera", frame)
 
