ddb53d9725c5e3c6995626936c159de2480de88a
Aidan Lincke <22aidanl@students.harker.org>
2021-07-28 12:41:16 -0700

    PID tuning

--- track_ball.py
+++ track_ball.py
@@ -6,12 +6,14 @@ import numpy as np
 
 tello = Tello()
 tello.connect()
-
 tello.takeoff()
 # tello.move("up", 50)
 tello.streamon()
 leftRightPID = PIDController(0.1, 0, 0.0001, 0.001)
 upDownPID = PIDController(0.2, 0, 0, 0.001)
+forwardBackwardPID = PIDController(0.06, 0, 0.0001, 0.001)
+
+initialArea = None
 
 detector = ColorAndContourDetector()
 
@@ -19,12 +21,19 @@ while True:
     frame = tello.get_frame_read().frame
     if frame is not None:
         ball = detector.detect(frame)
-
         if ball.centroid:
+        
             cv2.circle(frame, ball.centroid, 5, (255, 0, 0), thickness=10)
             leftRight = int(leftRightPID.next(ball.centroid[0] - 480))
             upDown = int(upDownPID.next(360 - ball.centroid[1]))
-            tello.send_rc_control(leftRight, 0, upDown, 0)
+            if initialArea is None:
+                initialArea = ball.area
+                forwardBackward = 0
+            else:
+                forwardBackward = int(forwardBackwardPID.next(initialArea - ball.area))
+            tello.send_rc_control(leftRight, forwardBackward, upDown, 0)
+            
+        # Uncomment for drone to stop when ball is out of frame
         # else:
             # tello.send_rc_control(0, 0, 0, 0)
         cv2.imshow("Tello Camera", frame)
