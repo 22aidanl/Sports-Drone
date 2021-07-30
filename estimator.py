7a806ec7e5f118b7081059c02ee9f51b68dd66d1
Andrew (Andy) Min <me@andrewmin.info>
2021-07-30 00:47:40 -0700

    Add ball estimator

---
+++ estimator.py
@@ -0,0 +1,19 @@
+from detection import Ball
+
+class BallEstimator:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.radius = 0
+        self.dx = 0
+    
+    def update(self, ball, dt):
+        self.dx = (ball.centroid[0] - self.x) / dt
+        self.x = ball.centroid[0]
+        self.y = ball.centroid[1]
+        self.radius = ball.radius
+
+    def estimate(self, dt):
+        # Only estimates x direction for now
+        self.x = self.x + self.dx * dt
+        return Ball((self.x, self.y), self.radius)
--- track_ball.py
+++ track_ball.py
@@ -1,36 +1,42 @@
 from pid import PIDController
 from detection import ColorAndContourDetector
+from estimator import BallEstimator
 from djitellopy import Tello
 import cv2
+
+PERIOD = 1 # ms
 
 tello = Tello()
 tello.connect()
 tello.takeoff()
 # tello.move("up", 50)
 tello.streamon()
-leftRightPID = PIDController(0.1, 0, 0.0001, 0.001)
-upDownPID = PIDController(0.2, 0, 0, 0.001)
-forwardBackwardPID = PIDController(1, 0, 0, 0.001)
+
+leftRightPID = PIDController(0.1, 0, 0.0001, PERIOD / 1000)
+upDownPID = PIDController(0.2, 0, 0, 0.001, PERIOD / 1000)
+forwardBackwardPID = PIDController(1, 0, 0, 0.001, PERIOD / 1000)
+
+ball_estimator = BallEstimator()
 
 while True:
     frame = tello.get_frame_read().frame
     if frame is not None:
         ball = ColorAndContourDetector.detect(frame)
-
-        if ball.centroid:
-            leftRight = int(leftRightPID.next(ball.centroid[0] - 480))
-            upDown = int(upDownPID.next(360 - ball.centroid[1]))
-            # forwardBackward = int(forwardBackwardPID.next(35 - ball.radius))
-            forwardBackward = int(forwardBackwardPID.next(10 - ball.radius))
-            print(35 - ball.radius)
-            tello.send_rc_control(leftRight, forwardBackward, upDown, 0)
-            
-        # Uncomment for drone to stop when ball is out of frame
-        # else:
-        #     tello.send_rc_control(0, 0, 0, 0)
+        if ball.centroid is None:
+            ball = ball_estimator.estimate(ball, PERIOD / 1000)
+        else:
+            ball_estimator.update(ball, PERIOD / 1000)
+        
+        leftRight = int(leftRightPID.next(ball.centroid[0] - 480))
+        upDown = int(upDownPID.next(360 - ball.centroid[1]))
+        # forwardBackward = int(forwardBackwardPID.next(35 - ball.radius))
+        forwardBackward = int(forwardBackwardPID.next(10 - ball.radius))
+        print(35 - ball.radius)
+        tello.send_rc_control(leftRight, forwardBackward, upDown, 0)
+        
         cv2.imshow("Tello Camera", frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(PERIOD) & 0xFF == ord('q'):
             break
 
 tello.send_rc_control(0, 0, 0, 0)
